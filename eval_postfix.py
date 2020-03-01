ad639ea82e4e564ddb403f454625ccba71db163e
prateek-dutt <duttprateek@gmail.com>
2020-03-01 18:42:04 +0530

    Functioning Commit-Second

--- eval_postfix.py
+++ eval_postfix.py
@@ -6,13 +6,13 @@ class Stack:
 def pop(stack):
     ##Considering stack is not empty
     #function should return the value of top of the stack after popping
-    top = stack.top
+    top = stack.items[stack.top]
     stack.top=stack.top-1
     return top
 def push(stack,val):
     stack.top+=1
     stack.items.insert(stack.top,val)
-    return stack.top
+    return stack.items[stack.top]
 def oper(char,op1,op2):
     if char is '+':
         return(op1+op2)
@@ -27,6 +27,7 @@ def eval(expr):
         else:
             #character is an operator
             opnd2=pop(open_stack)
+
             opnd1=pop(open_stack)
             value=oper(char,opnd1,opnd2)
             push(open_stack,value)
